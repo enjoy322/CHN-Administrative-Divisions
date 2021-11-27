670e51fb1675d366913a12a1343552f035c91ba0
theone322 <1693364356@qq.com>
2021-11-27 10:14:12 +0800

    feat: 1. 查询行政区划的上一级信息 2.查询行政区划的下一级所有信息

--- main.go
+++ main.go
@@ -12,6 +12,11 @@ func main() {
 	//crawler.County(file.CountyFile)
 	//crawler.Town(file.TownFile)
 	//crawler.Village(file.VillageFile)
+	fmt.Println(service.Service{}.GetByCode("530000000000"))
 	fmt.Println(service.GetProvinceByCode("530000000000"))
+	fmt.Println(service.Service{}.ListBelongingsByCode("530000000000"))
+	fmt.Println(service.Service{}.ListBelongingsByCode("532600000000"))
+	fmt.Println(service.Service{}.ListBelongingsByCode("532601001000"))
+	fmt.Println(service.Service{}.ListBelongingsByCode("532601001001"))
 
 }
--- service/common.go
+++ service/common.go
@@ -2,24 +2,101 @@ package service
 
 import (
 	"CHN-Administrative-Divisions/base"
-	"fmt"
 )
 
-func GetByCode(code string) (data base.Division) {
-	fmt.Println("code:", code)
+type method interface {
+	GetByCode(code string) (data base.Division)
+	ListBelongingsByCode(code string) (data []base.Division)
+	GetUpLevelDivisionByCode(code string) (data base.Division, f bool)
+}
+
+type Service struct {
+}
+
+func (s Service) GetByCode(code string) (data base.Division) {
 	switch level(code) {
-	case 2:
+	case base.CodeProvince:
 		data = GetProvinceByCode(code)
 		break
-	//case 4:
-	//	data = GetCityByCode(code)
-	//	break
-	//case 6:
-	//	return GetCountyByCode(code)
-	//case 9:
-	//	return GetTownByCode(code)
-	//case 12:
-	//	return GetVillageByCode(code)
+	case base.CodeCity:
+		data = GetCityByCode(code)
+		break
+	case base.CodeCounty:
+		return GetCountyByCode(code)
+	case base.CodeTown:
+		return GetTownByCode(code)
+	case base.CodeVillage:
+		return GetVillageByCode(code)
+	default:
+		panic("err")
+	}
+	return
+}
+
+func (s Service) ListBelongingsByCode(code string) (data []base.Division) {
+	levelInt := level(code)
+	switch levelInt {
+	case base.CodeProvince:
+		tempList := ListCity()
+		for _, division := range tempList {
+			if division.ProvinceCode == code {
+				data = append(data, division)
+			}
+		}
+		return
+	case base.CodeCity:
+		tempList := ListCounty()
+		for _, division := range tempList {
+			if division.CityCode == code {
+				data = append(data, division)
+			}
+		}
+		return
+	case base.CodeCounty:
+		tempList := ListTown()
+		for _, division := range tempList {
+			if division.CountyCode == code {
+				data = append(data, division)
+			}
+		}
+		return
+	case base.CodeTown:
+		tempList := ListVillage()
+		for _, division := range tempList {
+			if division.TownCode == code {
+				data = append(data, division)
+			}
+		}
+		return
+	case base.CodeVillage:
+		return
+	default:
+		panic("err")
+	}
+	return
+}
+
+func (s Service) GetUpLevelDivisionByCode(code string) (data base.Division, f bool) {
+	levelInt := level(code)
+	switch levelInt {
+	case base.CodeProvince:
+		return base.Division{}, false
+	case base.CodeCity:
+		temp := GetCityByCode(code)
+		data = GetProvinceByCode(temp.ProvinceCode)
+		return
+	case base.CodeCounty:
+		temp := GetCountyByCode(code)
+		data = GetCityByCode(temp.CityCode)
+		return
+	case base.CodeTown:
+		temp := GetTownByCode(code)
+		data = GetCountyByCode(temp.CountyCode)
+		return
+	case base.CodeVillage:
+		temp := GetVillageByCode(code)
+		data = GetTownByCode(temp.TownCode)
+		return
 	default:
 		panic("err")
 	}
@@ -35,6 +112,18 @@ func level(code string) int {
 			break
 		}
 	}
-	fmt.Println(12 - count)
-	return 12 - count
+	switch 12 - count {
+	case 2:
+		return base.CodeProvince
+	case 4:
+		return base.CodeCity
+	case 6:
+		return base.CodeCounty
+	case 9:
+		return base.CodeTown
+	case 12:
+		return base.CodeVillage
+	default:
+		panic("err")
+	}
 }
